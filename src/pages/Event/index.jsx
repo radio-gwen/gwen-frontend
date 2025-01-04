e72642bb3ba7ebc13248128840966d1e212febfa
murieru <murielhediger@gmail.com>
2025-01-04 10:55:38 +0100

    added fteching events and trakc on Event page

--- src/pages/Event/index.jsx
+++ src/pages/Event/index.jsx
@@ -18,14 +18,49 @@ const Event = () => {
 
     const {eventId} = useParams()
 
+    const {data: eventsData, isLoading: isEventLoading} = useFetch(`http://localhost:8000/events`)
+    const eventsList = eventsData?.eventsList || []
+
+    const {data: tracksData, isLoading: isTracksLoading} = useFetch(`http://localhost:8000/tracks`)
+    const tracksList = tracksData?.tracksList || []
+
+    if (isEventLoading) {
+        return <div>Loading...</div>; // Add a loading indicator
+    }
+
+    if (isTracksLoading) {
+        return <div>Loading...</div>; // Add a loading indicator
+    }
+
     const event = eventsList.find((t) => t.id === String(eventId))
 
+    if (!event) {
+        console.log('event not found!')
+    }
 
+    const tracks = tracksList.filter( (t) => t.mainId === String(eventId) && t.type === 'event' )
 
     return (
         <Section>
             <h1>{event.title}</h1>
             <Carousel image={''} />
+            <Card
+            title = {event.title}
+            desc = {event.desc}
+            text = {event.text}
+            btnContent = 'Back'
+            url = '/'
+            />
+
+            {tracks.map( (track) =>    
+                <div>
+                    <div className='line'></div>
+                    <Toogle title={track.name} id={`track-${track.id}`}>
+                        <Player track = {track.file}/>
+                    </Toogle>
+                </div>
+            )}
+
         </Section>
     )
 }
